5724a26b4359923a246e2503bd33dbf9305513dc
TonyInBeijing <yuehaowei1126@gmail.com>
2024-11-13 18:13:37 +0800

    homework-20240922

--- src/agents/agent_base.py
+++ src/agents/agent_base.py
@@ -55,10 +55,15 @@ class AgentBase(ABC):
         ])
 
         # 初始化 ChatOllama 模型，配置参数
+        # self.chatbot = system_prompt | ChatOllama(
+        #     model="llama3.1:8b-instruct-q8_0",  # 使用的模型名称
+        #     max_tokens=8192,  # 最大生成的 token 数
+        #     temperature=0.8,  # 随机性配置
+        # )
         self.chatbot = system_prompt | ChatOllama(
-            model="llama3.1:8b-instruct-q8_0",  # 使用的模型名称
-            max_tokens=8192,  # 最大生成的 token 数
-            temperature=0.8,  # 随机性配置
+            model="llama3.1",
+            max_tokens=8192,
+            temperature=0.8
         )
 
         # 将聊天机器人与消息历史记录关联
